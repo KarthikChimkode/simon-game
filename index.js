8c6821f7a3670572a7f612039f28321a2cbbdac5
Karthik Chimkode <99889801+KarthikChimkode@users.noreply.github.com>
2024-06-20 22:24:59 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,83 @@
+// $("h1").addClass("big-title  margin-50")
+// $("h1").text("Bye")
+
+// $("button").text("Don't Click me");
+
+// $("button").html("<em>Hey</em>")
+
+// $("button").text("<em>Hey</em>")
+
+// console.log($("img").attr("src"))
+
+// $("a").attr("href", "https://www.yahoo.com")
+
+// $("h1").click(function() {
+//     $("h1").css("color", "purple");
+// });
+
+// $("button").click(function() {
+//     $("h1").css("color", "purple");
+// })
+
+// $("input").keydown(function(event) {
+//     console.log(event.key)
+// })
+
+
+
+// $("Body").keydown(function(event) {
+//     $("h1").text(event.key)
+// })
+
+// $("h1").on("click", function() {
+//     $("h1").css("color", "purple")
+// })
+
+// $("h1").on("click", function() {
+//     $(this).css({
+//         "color": "purple",
+//         "text-align": "center"
+//     });
+// });
+
+// $("h1").before("<button>New</button>");
+
+// $("h1").prepend("<button>New</button>");
+
+// $("h1").append("<button>New</button>");
+
+// $("h1").after("<button>New</button>");
+
+// $("button").on("click", function() {
+//     $("h1").toggle();
+// })
+
+// $("button").on("click", function() {
+//     $("h1").fadeOut();
+// })
+
+// $("button").on("click", function() {
+//     $("h1").fadeIn();
+// })
+
+// $("button").on("click", function() {
+//     $("h1").slideUp();
+// })
+
+// $("button").on("click", function() {
+//     $("h1").slideDown();
+// })
+
+// $("button").on("click", function() {
+//     $("h1").slideToggle();
+// })
+
+// $("button").on("click", function() {
+//     $("h1").animate({opacity: 0.5});
+// })
+
+/*Chained*/
+
+// $("button").on("click", function() {
+//     $("h1").slideUp().slideDown().animate({opacity: 0.5});
+// })
